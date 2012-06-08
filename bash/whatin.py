94f2c92b239fd16ed119c9f1f83cb4f32ca47dcf
Dan Guest <dguest@cern.ch>
2012-06-08 13:57:50 +0200

    add some help

--- bash/whatin.py
+++ bash/whatin.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python2.7
-
+"""
+Dump info on root file. 
+"""
 if __name__ == "__main__":
     import sys, argparse
     from dumproot.parser import getFileFrom, dump
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description = __doc__, 
+        epilog = 'Author: Dan Guest <dguest@cern.ch>')
 
-    parser.add_argument('file_name', nargs='+')
+
+    parser.add_argument(
+        'file_name', nargs='+', 
+        help = "NOTE: must go before '-e' for tab-complete to work")
     parser.add_argument('-e', action = 'append', default = [], 
                         help = 'expand object in root file')
 
